a07f64c92e5495926d88b8f4a1c304570f09f961
Joe McCaffrey <mccaffre@umass.edu>
2015-10-15 16:58:53 -0400

    adding more directories and a blank readme

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var handlebars = require('experess-handlebars');
+
+
+var app = express();
+
+
+//setup handlebars
+
+// setup handlebars
+var view = handlebars.create({ defaultLayout: 'main' });
+app.engine('handlebars', view.engine);
+app.set('view engine', 'handlebars');
+
+// setup static serving
+app.use(express.static(__dirname + '/public'));
+
+
+app.use((req, res) => {
+        res.status(404);
+	res.render('404');
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+//// Application startup
+
+app.listen(app.get('port'), () => {
+	console.log('Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate');
+																						
+});
+
+
+
